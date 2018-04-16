d5bc0dd0b861a17321096d8d705b916e86d27162
Viraj <salvi.viraj007@gmail.com>
2018-04-16 01:01:39 -0400

    Adding Home work 8

---
+++ Fibonacci/fibonacci.java
@@ -0,0 +1,27 @@
+class fibonacci
+{
+    static int fib(int n)
+    {
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+          System.out.println(a);
+        }
+  
+        return b;
+    }
+    
+    
+    public static void main (String args[])
+    {
+        int n = 100;
+        System.out.println(fib(n));
+    }
+    
+    
+}
