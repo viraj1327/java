9da8d9b283db09be493692b58f4638abc23e1bb4
Viraj <salvi.viraj007@gmail.com>
2018-05-11 16:10:21 -0400

    changes

--- Final_2/machinetest.java
+++ Final_2/machinetest.java
@@ -4,23 +4,16 @@ import java.util.Scanner;
 public class machinetest {
 	public static void main(String args[]) {
 		machine mac = new machine();
-		//Ready re = new Ready(mac);
-		//Disabled db = new Disabled(mac);
-		//Unkown u = new Unkown(mac);
-		//Down d = new Down(mac);
-		//System.out.println("You are in the Unkown state");
-		mac.currentstate();
+		state ready, down, disbaled,unknown = null;
+		System.out.println("You are in the Unkown state");
+
+		mac.setState(unknown);
+		mac.getstate();
+		
+		while(true) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter one of the following : \ncheck is good\ncheck is bad\nservice enabled\nservice disabled");
-		while (true) {
-			//String a = input.nextLine();
-		
-		if (mac.perform(input.nextLine())) {
-			mac.currentstate();
-			}
-		//else {System.out.print("Check the input");}
-		}
-		//input.close();
-		}
-	
+		String s = input.nextLine();
+		mac.doAction(s);
+		}	
 }
